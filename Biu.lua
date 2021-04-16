02c9d393f8df371c0bf8eb15c107775793a8c2ec
laishuan <laishuan005@gmail.com>
2021-04-16 19:16:26 +0800

    commit

--- Biu.lua
+++ Biu.lua
@@ -1,6 +1,7 @@
 local util = {}
 local unpack = table.unpack or unpack
 local pack = table.pack or function(...) return { n = select('#', ...), ... } end
+
 local curry = function (f)
 	return function (...)
 		local args = {...}
@@ -8,6 +9,22 @@ local curry = function (f)
 			return f(observerable, unpack(args))
 		end
 	end
+end
+
+local compose = function (f1, f2)
+	return function (observerable)
+		f2(f1(observerable))
+	end
+end
+
+local joinNextFinish = function (onNext, onFinish)
+	local isFinsh = false
+	return {function ( ... )
+		if not isFinsh then onNext(...) end
+	end, function ( ... )
+		isFinsh = true
+		finish()
+	end}
 end
 
 util.unpack = unpack
@@ -21,54 +38,76 @@ util.constant = function(x) return function() return x end end
 local op = {}
 
 op.map = curry(function (observerable, f)
-	return function (next, finish)
-		observerable(function (...)
-			next(f(...))
+	return function (onNext, finish)
+		return observerable(function (...)
+			onNext(f(...))
 		end, finish)
 	end
 end)
 
 op.select = curry(function (observerable, f)
-	return function (next, finish)
-		observerable(function (...)
+	return function (onNext, finish)
+		return observerable(function (...)
 			if f(...) then
-				next(...)
+				onNext(...)
 			end
 		end, finish)
 	end
 end)
 
 op.reject = curry(function (observerable, f)
-	return function (next, finish)
-		observerable(function (...)
+	return function (onNext, finish)
+		onNext, finish = 
+		return observerable(function (...)
 			if not f(...) then
-				next(...)
+				onNext(...)
 			end
 		end, finish)
 	end
 end)
 
 op.reduce = curry(function (observerable, f, state)
-	return function (next, finish)
-		observerable(function (...)
+	return function (onNext, finish)
+		local first = true
+		return observerable(function (...)
 			local args = {...}
-			state = state or args[1]
-			state = f(state, ...)
+			if first then
+				state = state or args[1]
+				first = false
+			else
+				state = f(state, ...)
+			end
 		end, function ( ... )
-			next(state)
+			onNext(state)
 			finish()
 		end)
 	end
 end)
 
+op.scan = curry(function (observerable, f, state)
+	return function (onNext, finish)
+		local first = true
+		return observerable(function (...)
+			local args = {...}
+			if first then
+				state = state or args[1]
+				first = false
+			else
+				state = f(state, ...)
+			end
+			onNext(state)
+		end, finish)
+	end
+end)
+
 op.reverse = curry(function (observerable)
-	return function (next, finish)
+	return function (onNext, finish)
 		local arr = {}
-		observerable(function (...)
+		return observerable(function (...)
 			table.insert(arr, 1, {...})
 		end, function ( ... )
 			for i=#arr,1 do
-				next(unpack(arr[i]))
+				onNext(unpack(arr[i]))
 			end
 			finish()
 		end)
@@ -76,14 +115,14 @@ op.reverse = curry(function (observerable)
 end)
 
 op.findIndex = curry(function (observerable, f)
-	return function (next, finish)
+	return function (onNext, finish)
 		local index
 		local newFinish = function ( ... )
-			next(index)
+			onNext(index)
 			finish()
 		end
 
-		observerable(function ( ... )
+		return observerable(function ( ... )
 			if not index and f(...) then
 				local args = {...}
 				index = args[2]
@@ -93,14 +132,14 @@ op.findIndex = curry(function (observerable, f)
 end)
 
 op.findValue = curry(function (observerable, f)
-	return function (next, finish)
+	return function (onNext, finish)
 		local value
 		local newFinish = function ( ... )
-			next(value)
+			onNext(value)
 			finish()
 		end
 
-		observerable(function ( ... )
+		return observerable(function ( ... )
 			if not value and f(...) then
 				local args = {...}
 				value = args[1]
@@ -110,14 +149,14 @@ op.findValue = curry(function (observerable, f)
 end)
 
 op.all = curry(function (observerable, f)
-	return function (next, finish)
+	return function (onNext, finish)
 		local value = true
 		local newFinish = function ( ... )
-			next(value)
+			onNext(value)
 			finish()
 		end
 
-		observerable(function ( ... )
+		return observerable(function ( ... )
 			if value and not f(...) then
 				value = false
 			end
@@ -126,14 +165,14 @@ op.all = curry(function (observerable, f)
 end)
 
 op.groupBy = curry(function (observerable, f)
-	return function (next, finish)
+	return function (onNext, finish)
 		local _t = {}
 		local newFinish = function ( ... )
-			next(_t)
+			onNext(_t)
 			finish()
 		end
 
-		observerable(function ( ... )
+		return observerable(function ( ... )
 			local args = {...}
 			local v, k = args[1], args[2]
 		    local _key = f(v,k)
@@ -145,14 +184,14 @@ op.groupBy = curry(function (observerable, f)
 end)
 
 op.countBy = curry(function (observerable, f)
-	return function (next, finish)
+	return function (onNext, finish)
 		local _t = {}
 		local newFinish = function ( ... )
-			next(_t)
+			onNext(_t)
 			finish()
 		end
 
-		observerable(function ( ... )
+		return observerable(function ( ... )
 			local args = {...}
 			local v, i = args[1], args[2]
 		    local key = f(v,i)
@@ -161,40 +200,106 @@ op.countBy = curry(function (observerable, f)
 	end
 end)
 
+op.max = function ()
+	return op.reduce(math.max)
+end
+
+op.min = function ()
+	return op.reduce(math.min)
+end
+
+op.sample = curry(function (observerable, sampler)
+	return function (onNext, finish)
+		local values
+
+		local stop1 = observerable(function ( ... )
+			values = {...}
+		end, util.noop)
+
+		local stop2 = sampler(function ( ... )
+			if values ~= nil then
+				onNext(unpack(values))
+			end
+		end, finish)
+
+		return function ( ... )
+			if stop1 then stop1() end
+			if stop2 then stop1() end
+		end
+	end
+end)
+
+op.flatten = curry(function (observerable)
+	return function (onNext, finish)
+	    local subscriptions = {}
+	    local remaining = 1
+
+	    local function newFinish()
+	      remaining = remaining - 1
+	      if remaining == 0 then
+	        finish()
+	      end
+	    end
+		observerable(function (v)
+			subscriptions[#subscriptions+1] = v(function ( ... )
+				remaining = remaining
+				onNext(...)
+			end, util.noop)
+		end, newFinish)
+
+		return function ( ... )
+	      for i = 1, #subscriptions do
+	        subscriptions[i]()
+	      end
+		end
+	end
+end)
+
+op.flatMap = curry(function (observerable, f)
+	return compose(op.map(f), op.flatten())(observerable)
+end)
+
+op.startWith = function (observerable, v)
+	return function (onNext, finish)
+		onNext(v)
+		return observerable(onNext, finish)
+	end
+end
+
 local Biu = {}
 
 Biu.util = util
 Biu.op = op
 
 function Biu:of(v)
-	return function (next, finish)
-		next(v)
+	return function (onNext, finish)
+		onNext(v)
 		finish()
 	end
 end
 
 function Biu:fromArr(v)
-	return function (next, finish)
+	return function (onNext, finish)
 		for i,v in ipairs(v) do
-			next(v,i)
+			onNext(v,i)
 		end
 		finish()
 	end
 end
 
 function Biu:fromObj(v)
-	return function (next, finish)
+	return function (onNext, finish)
 		for k,v in pairs(v) do
-			next(v, k)
+			onNext(v, k)
 		end
 		finish()
 	end
 end
 
 function Biu:fromRange(from, to)
-	return function (next, finish)
+	return function (onNext, finish)
 		for i=from,to do
-			next(i,i)
+			onNext(i,i)
 		end
 		finish()
 	end
