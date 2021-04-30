389bf393af31c5a04f4221aa39310817a8c15859
laishuan <laishuan005@gmail.com>
2021-05-01 02:09:32 +0800

    util test ok op ok without test

---
+++ Op.js
@@ -0,0 +1,509 @@
+op = {}
+let curry = f=>(...args)=>(observerable)=>{
+    return f(observerable, ...args)
+}
+
+let compose = (f1, f2)=>observerable=>{
+    return f2(f1(observerable))
+}
+
+let createOBf = f=>(onNext, onFinish)=>{
+    let isFinish = false
+    let newNext = (...args)=>{
+        if (!isFinish) {onNext(...args)}
+    }
+    let newFinish = (...args)=>{
+        isFinish = true
+        if (onFinish) onFinish(...args)
+    }
+    return f(newNext, newFinish)
+}
+
+op.next = f=>observerable=>createOBf((onNext, onFinish)=>observerable(util.noop, (...args)=>{
+        createOBf((innerOnNext, innerOnFinish)=>{
+            f(innerOnFinish, ...args)
+        })(util.noop, onFinish)
+    })
+)
+op.dump = prefix=>observerable=>createOBf((onNext, onFinish)=>observerable((...args)=>{
+        prefix = prefix || ""
+        util.dump(args, "op_dump_next:"+prefix)
+        onNext(...args)
+    }, (...args)=>{
+        util.dump(args, "op_dump_finish:"+prefix)
+        onFinish(...args)
+
+    })
+)
+op.print = prefix=>observerable=>createOBf((onNext, onFinish)=>observerable((...args)=>{
+        prefix = prefix || ""
+        console.log(prefix + "_next", ...args)
+        onNext(...args)
+    }, (...args)=>{
+        console.log(prefix + "_finish", ...args)
+        onFinish(...args)
+
+    })
+)
+op.tap = f=>observerable=>createOBf((onNext, onFinish)=>observerable((...args)=>{
+        f(...args)
+        onNext(...args)
+    },onFinish)
+)
+op.pip = (...args)=>{
+    let f = util.identity
+    return observerable=>{
+        args.forEach((v)=>{
+            f=compose(f, v)
+        })
+        return f(observerable)
+    }
+}
+op.map = f=>observerable=>createOBf((onNext, onFinish)=>observerable((...args)=>{
+        onNext(f(...args))
+    }, onFinish)
+)
+
+op.select = f=>observerable=>createOBf((onNext, onFinish)=>observerable((...args)=>{
+    if (f(...args)) onNext(...args)
+},onFinish)
+)
+
+op.filter=op.select
+
+op.reject = f=>observerable=>createOBf((onNext, onFinish)=>observerable((...args)=>{
+    if (!f(...args)) onNext(...args)
+},onFinish)
+)
+
+op.reduce =(f,state)=>observerable=>createOBf((onNext, onFinish)=>{
+    let first = true
+    return observerable((...args)=>{
+        if (first) {
+            if (state !== undefined) {
+                state = f(state, ...args)
+            }
+            else
+                state = args[0]
+            first = false 
+        }
+        else {
+            state = f(state, ...args)
+        }
+    }, (...args)=>{
+        onNext(state)
+        onFinish(...args)
+    })
+})
+op.scan = (f,state)=>observerable=>createOBf((onNext, onFinish)=>{
+    let first = true
+    return observerable((...args)=>{
+        if (first) {
+            if (state !== undefined) {
+                state = f(state, ...args)
+            }
+            else
+                state = args[0]
+            first = false 
+        }
+        else {
+            state = f(state, ...args)
+        }
+        onNext(state, args[1])
+    }, onFinish)
+})
+op.reverse = ()=>observerable=>createOBf((onNext, onFinish)=>{
+    let arr = []
+    return observerable((...args)=>{
+        arr.push(args)
+    }, (...args)=>{
+        arr.reverse().forEach((v,i)=>{
+            onNext(v, i+1)
+        })
+        onFinish(...args)
+    })
+})
+op.findIndex = f=>observerable=>createOBf((onNext, onFinish)=>{
+    let index
+    let newFinish = (...args)=>{
+        onNext(index)
+        onFinish(...args)
+    }
+    return observerable((...args)=>{
+        if (!index && f(...args)) {
+            index = args[1]
+        }
+    }, newFinish)
+})
+op.findValue = value=>observerable=>createOBf((onNext, onFinish)=>{
+    let isFind = false
+    let newFinish = (...args)=>{
+        onNext(isFind)
+        onFinish(...args)
+    }
+    return observerable((...args)=>{
+        if (!isFind && value === args[0]) isFind = true
+    }, newFinish)
+})
+
+op.all = f=>observerable=>createOBf((onNext, onFinish)=>{
+    let value = true
+    let newFinish = (...args)=>{
+        onNext(value)
+        onFinish(...args)
+    }
+    return observerable((...args)=>{
+        if (value && !f(...args)) value = false
+    }, newFinish)
+})
+
+op.groupBy = f=>observerable=>createOBf((onNext, onFinish)=>{
+    let _t = {}
+    let newFinish = (...args)=>{
+        onNext(_t)
+        onFinish(...args)
+    }
+    return observerable((...args)=>{
+        let [v,k] = args
+        let _key = f(v,k)
+        if (_t[_key]) _t[_key].push(v)
+        else _t[_key]=[v]
+    }, newFinish)
+})
+
+op.countBy = f=>observerable=>createOBf((onNext, onFinish)=>{
+    let _t = {}
+    let newFinish = (...args)=>{
+        onNext(_t)
+        onFinish(...args)
+    }
+    return observerable((...args)=>{
+        let [v,i] = args
+        let _key = f(v,k)
+        _t[_key] = (_t[_key] || 0) + 1
+    }, newFinish)
+})
+
+op.max = ()=>op.reduce((state, v)=>Math.max(state,v))
+
+op.min = ()=>op.reduce((state, v)=>Math.min(state,v))
+
+op.sum = ()=>op.reduce((x,y)=>x+y, 0)
+
+op.value = ()=>{
+    let first = true
+    let value = {"firstNoK": false}
+    return op.reduce((state, v, k)=>{
+        if (first) {
+            value.firstNoK = (k==undefined)
+            first = false
+        }
+        let data = value.data
+        if (data === undefined) {
+            if (k === undefined || util.type(k) === "Number") data = []
+            else data = {}
+        }
+        value.data = data
+        if (util.type(data) === "Object") data[k] = v
+        else if (util.type(data) === "Array") data.push(v)
+        return value
+    }, value)
+}
+
+op.sample = sampler=>observerable=>createOBf((onNext, onFinish)=>{
+    let values
+    let stop1 = observerable((...args)=>{
+        values = args
+    }, util.noop)
+
+    let stop2 = sampler((...args)=>{
+        if (values !== undefined) {
+            onNext(...values)
+        }
+    }, onFinish)
+
+    return () => {
+        if (stap1) stop1()
+        if (stap2) stop2()
+    }
+})
+
+op.flatten = f=>observerable=>createOBf((onNext, onFinish)=>{
+    let subscriptions = []
+    let remaining = 1
+    let newFinish = (...args)=>{
+        remaining = remaining - 1
+        if (remaining === 0) {
+            onFinish(...args)
+        }
+    }
+    observerable((v)=>{
+        subscriptions.push(v((...args)=>{
+            remaining = remaining + 1
+            onNext(...args)
+        }, util.noop))
+    }, newFinish)
+
+    return ()=>{
+        subscriptions.forEach((v)=>{
+            v()
+        })
+    }
+})
+
+op.flatMap = f=>op.pip(op.map(f), op.flatten())
+
+op.switch = ()=>observerable=>createOBf((onNext, onFinish)=>{
+    let innerSubscription
+    let switchOB = (source)=>{
+        if (innerSubscription !== undefined)
+            innerSubscription()
+        innerSubscription = source(onNext)
+    } 
+    let subscription = observerable(switchOB, onFinish)
+    return ()=>{
+        if (innerSubscription) innerSubscription()
+        if (subscription) subscription()
+    }
+})
+
+op.take = n=>observerable=>createOBf((onNext, onFinish)=>{
+    n = n || 1
+    if (n<=0) {
+        onFinish()
+        return
+    }
+    let i = 1
+    return observerable((...args)=>{
+        onNext(...args)
+        i = i + 1
+        if (i > n) {
+            onFinish()
+        }
+    },onFinish)
+})
+
+op.skip = n=>observerable=>createOBf((onNext, onFinish)=>{
+    n = n || 1
+    let i = 1
+    return observerable((...args)=>{
+        if (i>n) 
+            onNext(...args)
+        else
+            i = i + 1
+    },onFinish)
+})
+
+op.get = (k, ...args)=>{
+    let getOne = (kk, first)=>observerable=>createOBf((onNext,onFinish)=>observerable((t)=>{
+        if (!util.isTable(t)) onNext(undefined)
+        else if (first) {
+            let v = first ? t : undefined
+            if (kk !== undefined) {
+                let realKK = util.type(kk) === "Number" ? kk-1 : kk
+                v = t[realKK]
+            }
+            onNext(v)
+        }
+    }, onFinish))
+    let arr = [getOne(k, true)]
+    args.forEach((v,i)=>{
+        arr.push(getOne(v))
+    })
+    arr.push(op.select((v)=>{
+        return v !== undefined
+    }))
+
+    return op.pip(...arr)
+}
+
+op.pluck = op.get
+
+op.first = ()=>op.take(1)
+
+op.buffer = size=>observerable=>createOBf((onNext, onFinish)=>{
+    if (!size || util.type(size) !== "Number") {console.assert(false, "expect a number")}
+    let buffer = []
+    let emit = ()=>{
+        if (buffer.length > 0) {
+            onNext(...buffer)
+            buffer = []
+        }
+    }
+
+    let newNext = (...args)=>{
+        args.forEach((v,i)=>{
+            buffer.push(v)
+            if (buffer.length >= size) {
+                emit()
+            }
+        })
+    }
+
+    let newFinish = (...args)=>{
+        emit()
+        onFinish(...args)
+    }
+    return observerable(newNext,newFinish)
+})
+
+op.join = (...sources)=>observerable=>createOBf((onNext, onFinish)=>{
+    sources.unshift(observerable)
+    let latest = []
+    let pending = sources.map((v)=>true)
+    let completed = []
+    let subscription = []
+
+    let newNext = (i)=>value=>{
+        latest[i]=value
+        pending[i]=false
+        if (pending.every((v,i)=>!v)) {
+            onNext(...latest)
+        }
+    }
+    
+    let newFinish = (i)=>(...args)=>{
+        completed.push(i)
+        if (completed.length === sources.length)
+            onFinish(...args)
+    }
+
+    sources.forEach((v,i)=>{
+        subscription.push(sources[i](newNext(i), newFinish(i)))
+    })
+
+    return ()=>{
+        subscription.forEach((v)=>{
+            v()
+        })
+    }
+})
+
+op.change = comparator=>observerable=>createOBf((onNext, onFinish)=>{
+    comparator = comparator || util.eq
+    let first = true
+    let currentValue = undefined
+
+    let newNext = (value, ...args)=>{
+        if (first || !comparator(value, currentValue)) {
+            onNext(value, ...args)
+            currentValue = value
+            first = false
+        }
+    }
+    return observerable(newNext, onFinish)
+})
+
+op.tp = tp=>observerable=>createOBf((onNext, onFinish)=>{
+    return observerable((...args)=>{
+        if (args[0] === tp) {
+            args.shift()
+            return onNext(...args)
+        }
+    }, onFinish)
+})
+
+op.changeFrom = v=>{
+    let lastAndCur = op.window(2)
+    return op.pip(lastAndCur, op.filter((v1,v2)=>{
+        if (util.type(v) === "Function")
+            return v(v1)
+        else 
+            return v1 === v
+    }))
+}
+
+op.changeTo = v=>{
+    if (util.type(v) !== "Function") {
+        return op.pip(op.change(), op.select((vv)=>vv==v))
+    }
+    else return op.pip(op.change(), op.select(v))
+}
+
+op.flatMapLatest = callback=>observerable=>createOBf((onNext, onFinish)=>{
+    callback = callback || util.identity
+    let innerSubscription
+
+    let subscribeInner = (...args)=>{
+        if (innerSubscription)
+            innerSubscriptionI()
+        innerSubscription = callback(...args)(onNext)
+    }
+
+    let subscription = observerable(subscribeInner, onFinish)
+
+    return ()=>{
+        if (innerSubscription) innerSubscription()
+        if (subscription) subscription()
+    }
+})
+
+op.merge = (...sources)=>observerable=>createOBf((onNext, onFinish)=>{
+    sources.unshift(observerable)
+    let completed = []
+    let subscription = []
+    
+    let newFinish = (i)=>(...args)=>{
+        completed.push(i)
+        if (completed.length === sources.length)
+            onFinish(...args)
+    }
+
+    sources.forEach((v,i)=>{
+        subscription.push(sources[i](onNext, newFinish(i)))
+    })
+
+    return ()=>{
+        subscription.forEach((v)=>{
+            v()
+        })
+    }
+})
+
+op.window = size=>observerable=>createOBf((onNext, onFinish)=>{
+    if (size === undefined || util.type(size) !== "Number") {console.assert(false, "expect a number")}
+    let window = []
+    let newNext = value=>{
+        window.push(value)
+        if (window.length >= size) {
+            onNext(...window)
+            window.shift()
+        }
+    }
+    return observerable(newNext, onFinish)
+})
+
+op.with = (...sources)=>observerable=>createOBf((onNext, onFinish)=>{
+    let latest = []
+    let subscription = []
+    let setLatest = i=>value=>{
+        latest[i] = value
+    }
+    let newNext = value=>onNext(value, ...latest)
+
+    sources.forEach((v,i)=>{
+        subscription.push(v(setLatest(i), util.noop))
+    })
+
+    subscription.push(observerable(newNext, onFinish)) 
+    return ()=>{
+        subscription.forEach(v=>{if(v) v()})
+    }
+})
+
+op.startWith = (...args)=>observerable=>createOBf((onNext, onFinish)=>{
+    onNext(...args)
+    return observerable(onNext, onFinish)
+})
+
+op.concat = (other, ...others)=>{
+    let concatOne = o=>observerable=>createOBf((onNext, onFinish)=>observerable(util.noop, (...args)=>{
+            return o(util.noop, onFinish)
+        })
+    )
+    let arr = [concatOne(other)]
+    others.forEach((v,i)=>{
+        arr.push(concatOne(v))
+    })
+    return op.pip(...arr)
+}
---
+++ Util.js
@@ -0,0 +1,118 @@
+util = {}
+let print = console.log
+let type = (t) => {
+    let str =  Object.prototype.toString.call(t)
+    let arr = /\[object (\w+)\]/.exec(str)
+    return arr[1]
+}
+
+let isTable = (t) => {
+    let tp = type(t)
+    return tp === "Array" || tp === "Object"
+}
+
+let loopKes = (t)=>{
+    console.assert(isTable(t), "t must be object or array")
+    let keys = util.keys(t)
+    return keys.map((k)=>{
+        let kk = type(k) === "Number" ? k-1 : k
+        let vv = t[kk]
+        return [vv, kk]
+    })
+}
+util.isTable = isTable
+util.type = type
+util.loopKes = loopKes
+util.eq = (x,y)=>{return x===y}
+util.noop = ()=>{}
+util.identity = x=>x
+util.constant = x=>()=>x
+util.keys = obj=> {
+    let keys = Object.keys(obj)
+    if (type(obj) === "Array") {
+        return keys.map((v)=>parseInt(v)+1)
+    }
+    return keys
+}
+util.split =(str, c)=>str.split(c)
+
+let rptChar = (c, times)=>{
+    if (times<=0) return ""
+    return c + rptChar(c, times-1)
+}
+util.toStr = (v, deep) => {
+    deep = deep === undefined ? 0 : deep
+    if (!isTable(v)) {
+        return v.toString()
+    }
+    else {
+        let str = "{\n"
+        let keys = util.keys(v)
+        keys.forEach((k, i)=>{
+            let vv = type(k) === "Number" ? v[k-1] : v[k]
+            str = str + rptChar('\t', deep+1) + k + ":" + util.toStr(vv, deep+1) + "\n"
+        })
+        str = str + rptChar('\t', deep) + "}\n"
+        return str
+    }
+}
+
+util.diff = (t1, t2)=>{
+    if (!isTable(t1) || !isTable(t2))
+        return
+    if (type(t1) !== type(t2))
+        return
+    let ret
+    let getRet = (k)=>{
+        ret = ret || (type(k) === "Number" ? [] : {})
+        return ret
+    }
+    loopKes(t1).forEach(([v,k])=>{
+        print (v, k)
+        let v2 = t2[k]
+        if (type(v) !== type(v2)) {
+            if (v2 !== undefined) {
+                getRet(k)[k] = v2
+            }
+            else {
+                getRet(k)[k] = "_delete"
+            }
+        }
+        else if (isTable(v)) {
+            let curDiff = util.diff(v, v2)
+            if (curDiff !== undefined) {
+                getRet(k)[k] = curDiff
+            }
+        }
+        else if (v !== v2) {
+            getRet(k)[k] = v2
+        }
+    })
+
+    loopKes(t2).forEach(([v, k])=>{
+        if (t1[k] === undefined) {
+            getRet(k)[k] = v
+        }
+    })
+
+    return ret
+}
+
+util.dump = (t, prefix, deep) => {
+    if (prefix) {
+        console.log(prefix, util.toStr(t, deep))
+    }
+    else
+        console.log(util.toStr(t))
+}
+// console.log(util.toStr({"a":{"b":[1,2,3], "ddd": 123}}))
+// console.log(util.keys([123123,1,1,1,1]))
+// util.dump(loopKes({"a":1, "b":2}), "test loopKeys")
+// let diff = util.diff(
+//     {"a":{"b":[1,11,3], "ddd": 123}}, 
+//     {"a":{"b":[1,2,3], "ddd": 1123}}, 
+//     // [1, 2],[2]
+//     // {"a":1}, {"a": 2}
+// )
+// print(diff)
+// util.dump(diff, "test dump")
